d2bd588ed3c579eb2bb201c350ddd856c7b63831
Jerome, Gabe <gabriel.jerome@mines.sdsmt.edu>
2024-09-09 22:45:03 -0600

    Ref #1 Added identity tables to the database

---
+++ KROApp/KROApp.Server/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KROApp.Server.Models
+{
+  public class User : IdentityUser
+  {
+    public int Id;
+
+    public string Email { get; set; }
+  }
+}
